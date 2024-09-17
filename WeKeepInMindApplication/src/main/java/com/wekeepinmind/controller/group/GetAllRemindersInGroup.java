d875f0c911bb372c6d8b455c920ad8652c0dd88a
Mayank-Sharma-27 <mayank.sharma2796@gmail.com>
2024-09-17 06:19:27 -0700

    Remove imports

--- WeKeepInMindApplication/src/main/java/com/wekeepinmind/controller/group/GetAllRemindersInGroup.java
+++ WeKeepInMindApplication/src/main/java/com/wekeepinmind/controller/group/GetAllRemindersInGroup.java
@@ -7,7 +7,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
